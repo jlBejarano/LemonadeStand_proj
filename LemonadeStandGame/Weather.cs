f0014f77af0d39d8d6f9b25b1cad1beef355cf51
jorge bejarano <jorge.bejarano@yahoo.com>
2020-03-20 15:25:33 -0500

    working on weather class, dont know if im going to stay on the path i am but i might change it

---
+++ LemonadeStandGame/Weather.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStandGame
+{
+    public class Weather
+    {
+        public string weatherAtmosphere;
+        public string weatherCondition;
+        public int weatherTempurature;
+        Random random;
+
+        public void WeatherList()
+        {
+            List<string> weatherReport = new List<string>();
+            weatherReport.Add("Rain");
+            weatherReport.Add("Sunny");
+            weatherReport.Add("Cloudy");
+            weatherReport.Add("Thunderstorm");
+
+
+
+
+        }
+
+        
+        
+        
+
+            
+            
+               
+
+            
+              
+        
+            
+        
+        
+        
+        
+        
+
+        public Weather GetReport()
+        {
+
+        }
+        
+            
+        
+        
+        
+            
+            
+            
+        
+
+    }
+}
