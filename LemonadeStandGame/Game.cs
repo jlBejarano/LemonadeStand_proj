adb04f3e16cf60828fa2e8d663d801debbccdc6f
jorge bejarano <jorge.bejarano@yahoo.com>
2020-06-15 12:34:00 -0400

    final commit w/ new game methods

--- LemonadeStandGame/Game.cs
+++ LemonadeStandGame/Game.cs
@@ -17,6 +17,7 @@ namespace LemonadeStandGame
         int numberOfDays;
         double priceOfLemonade;
         string decideToPurchase;
+        string choiceOfPurchase;
 
         public Game()
         {
@@ -36,6 +37,7 @@ namespace LemonadeStandGame
             Console.WriteLine("Welcome to The Lemonade Stand Game!");
             Console.ReadLine();
             CreatePlayer();
+            DecideNumberOfDays();
             Console.WriteLine("To start day one press enter!");
             Console.ReadLine();
             for (int i = 1; i <= Days.Count; i++)
@@ -44,6 +46,8 @@ namespace LemonadeStandGame
                 Weather weather = new Weather();
                 newPlayer.inventory.DisplayInventory();
                 newPlayer.wallet.DisplayAmountOfMoney();
+                ChoiceOfPurchase();
+                ChooseWhatToBuy();
                 decideToPurchase = null;
                 Console.WriteLine($"Day {i} of {numberOfDays}");
                 Console.WriteLine($"\nTempurature:{weather.weatherCondition}\n");
@@ -69,15 +73,73 @@ namespace LemonadeStandGame
             Console.WriteLine("New player enter your name:");
             Console.ReadLine();
         }
-
-        public void GameMenu()
+        public void DecideNumberOfDays()
         {
-            Console.WriteLine("Enter option to start game");
-            Console.WriteLine($"\n1 Inventory \n2 Make recipe \n3 Store run \n4 Begin the day");
-
+            while (numberOfDays != 7 && numberOfDays != 14 && numberOfDays != 30)
+            {
+                Console.WriteLine($"{newPlayer.name},How many days to run the stand? \nType 7 for 7 days.\nType 14 for 14 days.\nType30 for 30 days.");
+                string userInput = Console.ReadLine();
+                if(userInput == "7" || userInput == "14" || userInput == "30")
+                {
+                    numberOfDays = Convert.ToInt32(userInput);
+                    for (int i = 0; i < numberOfDays; i++)
+                    {
+                        Day day = new Day();
+                        Days.Add(day);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Not valid option.");
+                }
+            }
         }
-       
 
+        public void ChoiceOfPurchase()
+        {
+            while (choiceOfPurchase != "1" && choiceOfPurchase != "2")
+            {
+                Console.WriteLine("What items would you like to purchase?");
+                choiceOfPurchase = Console.ReadLine();
+                if (choiceOfPurchase != "1" && choiceOfPurchase != "2")
+                {
+                    Console.WriteLine("Not Valid Option");
+                }
+            }
+        }
+
+        public void ChooseWhatToBuy()
+        {
+            while(choiceOfPurchase == "1")
+            {
+                newPlayer.wallet.DisplayAmountOfMoney();
+                newPlayer.inventory.DisplayInventory();
+                Console.WriteLine($"Type 1 to buy more lemons\nType 2 to purchase more Cups\nType 3 to purchase more sugar cubes\nType 4 to purchase more Ice Cubes");
+                string userInput = Console.ReadLine();
+
+                if (userInput == "1")
+                {
+                    store.SellLemons(newPlayer);
+                }
+                else if (userInput == "2")
+                {
+                    store.SellCups(newPlayer);
+                }
+                else if (userInput == "3")
+                {
+                    store.SellSugarCubes(newPlayer);
+                }
+                else if (userInput == "4")
+                {
+                    store.SellIceCubes(newPlayer);
+                }
+                else
+                {
+                    Console.WriteLine("Not Valid Option");
+                }   
+        
+            }
+        }
         public void Createcustomers()
         {
             for (int i = 0; i < 100; i++)
